a08e9d19b4de99e50a532a2b503f02d9a165e68a
EmmanuelETM <113480839+EmmanuelETM@users.noreply.github.com>
2022-11-04 22:19:22 -0400

    Update old.py

--- old.py
+++ old.py
@@ -1,11 +1,12 @@
+from colorama import Fore as F
 from tabulate import tabulate
 
 ls = [1,2,3,4,5,6,7,8,9]
 
-tabla = [
+tabla = sting([
 [ls[0], ls[1], ls[2]],
 [ls[3], ls[4], ls[5]],
 [ls[6], ls[7], ls[8]]
-]
+])
 
-print(tabulate(tabla))
+print(F.CYAN + tabulate(tabla))
